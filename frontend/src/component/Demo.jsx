f8deef5b743bd54872875845e21058069d7b2ef1
Tausif Shekh <nisargcontact2020@gmail.com>
2021-11-17 13:48:15 +0530

    make it better

--- frontend/src/component/Demo.jsx
+++ frontend/src/component/Demo.jsx
@@ -6,85 +6,87 @@ const Demo = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div className="f" style={{ backgroundColor: darkMode && "olive" }}>
-      <div className="container">
-        <div className="product">
-          <div className="effect-1"></div>
-          <div className="effect-2"></div>
-          <div className="content">
-            <div className="exercise">
-              <a
-                href="https://www.facebook.com/tcssuggest"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i>
-                  <FaFacebookF />
-                </i>
-              </a>
+    <>
+      <div className="f" style={{ backgroundColor: darkMode && "olive" }}>
+        <div className="container">
+          <div className="product">
+            <div className="effect-1"></div>
+            <div className="effect-2"></div>
+            <div className="content">
+              <div className="exercise">
+                <a
+                  href="https://www.facebook.com/tcssuggest"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i>
+                    <FaFacebookF />
+                  </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <div className="effect-1"></div>
+            <div className="effect-2"></div>
+            <div className="content">
+              <div className="sleep">
+                <a
+                  href="https://twitter.com/TAUSIFMUKTAR"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i>
+                    <FaTwitter />
+                  </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <div className="effect-1"></div>
+            <div className="effect-2"></div>
+            <div className="content">
+              <div className="sleep">
+                <a
+                  href="https://www.linkedin.com/in/tausif-sheikh-8b4117bb/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i>
+                    <FaLinkedin />
+                  </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <div className="effect-1"></div>
+            <div className="effect-2"></div>
+            <div className="content">
+              <div className="sleep">
+                <a
+                  href="https://github.com/TausifM"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i>
+                    <FaGithub />
+                  </i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="product">
-          <div className="effect-1"></div>
-          <div className="effect-2"></div>
-          <div className="content">
-            <div className="sleep">
-              <a
-                href="https://twitter.com/TAUSIFMUKTAR"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i>
-                  <FaTwitter />
-                </i>
-              </a>
-            </div>
+        <div className="f-contain">
+          <div className="f-contain-wrapper">
+            <h3>Contact</h3>
+            <br />
+            <p>+91 - 99757 67561</p>
+            <h3>Email</h3>
+            <br />
+            <p>heytausifsheikh@gmail.com</p>
           </div>
-        </div>
-        <div className="product">
-          <div className="effect-1"></div>
-          <div className="effect-2"></div>
-          <div className="content">
-            <div className="sleep">
-              <a
-                href="https://www.linkedin.com/in/tausif-sheikh-8b4117bb/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i>
-                  <FaLinkedin />
-                </i>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="product">
-          <div className="effect-1"></div>
-          <div className="effect-2"></div>
-          <div className="content">
-            <div className="sleep">
-              <a
-                href="https://github.com/TausifM"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i>
-                  <FaGithub />
-                </i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="f-contain">
-        <div className="f-contain-wrapper">
-          <h3>Contact</h3>
-          <br />
-          <p>+91 - 99757 67561</p>
-          <h3>Email</h3>
-          <br />
-          <p>heytausifsheikh@gmail.com</p>
         </div>
       </div>
       <div className="f-copy">
@@ -92,7 +94,7 @@ const Demo = () => {
           <h5 style={{ color: "brown" }}>Made with ♥ Tausif Sheikh</h5>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
